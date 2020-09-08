c79aba8abed3f95025e55d7c1fb4222dedfcb63b
Thomas Sweet <admin@intentionrepeater.com>
2020-09-08 18:16:25 -0500

    Add files via upload

--- intention_repeater.py
+++ intention_repeater.py
@@ -1,8 +1,10 @@
 #Intention Repeater created by Thomas Sweet
-#Updated 9/7/2020
+#Updated 9/8/2020 v5.2
 #Repeats intention a million or more times per second
 #Depending on the intensity chosen and the hardware capability.
 #Python script. Run using: python3 intention_repeater.py
+#Automated Example: python3 intention_repeater.py YYY8 my_intents.txt
+#Intention Repeater is powered by a Servitor (20 Years / 2000+ hours in the making)
 #Licensed under GNU General Public License v3.0
 #https://choosealicense.com/licenses/gpl-3.0/
 
@@ -18,25 +20,25 @@ def human_format(num):
         num /= 1000.0
     return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T', 'Q'][magnitude])
 
-print("Intention Repeater v5.1 software created by Thomas Sweet.\n")
+print("Intention Repeater v5.2 software created by Thomas Sweet.\n")
 print("This software comes with no guarantees or warranty of any kind.\n")
 
 args = list(sys.argv)
 
 try:
-  params = str.upper(args[1])
-  gutheartmindcoherenceparam = str.upper(params[0])
-  clearinterferenceparam = params[1]
-  akashicrecordsparam = params[2]
-  intensityparam = params[3]
-  filenameparam = str(args[2])
+    params = str.upper(args[1])
+    gutheartmindcoherenceparam = str.upper(params[0])
+    clearinterferenceparam = params[1]
+    akashicrecordsparam = params[2]
+    intensityparam = params[3]
+    filenameparam = str(args[2])
 except:
-  params = ''
-  gutheartmindcoherenceparam = ''
-  clearinterferenceparam = ''
-  akashicrecordsparam = ''
-  intensityparam = ''
-  filenameparam = ''
+    params = ''
+    gutheartmindcoherenceparam = ''
+    clearinterferenceparam = ''
+    akashicrecordsparam = ''
+    intensityparam = ''
+    filenameparam = ''
 
 list_write = []
 
@@ -60,13 +62,13 @@ else:
 
 if clearinterferenceparam == '':
     while clear_interference != 'Y' and clear_interference != 'N':
-            clear_interference = str.upper(input("Clear Interference? [More effective, but can be exhausting] (Y/N): "))
+        clear_interference = str.upper(input("Clear Interference? [More effective, but can be exhausting] (Y/N): "))
 else:
     clear_interference = clearinterferenceparam
 
 if akashicrecordsparam == '':
     while akashic_records != 'Y' and akashic_records != 'N':
-        akashic_records = str.upper(input("Use Akashic Records? [Very expansive, and may be hard to focus down] (Y/N): "))
+        akashic_records = str.upper(input("Use Akashic Records? [Very expansive, but may be hard to focus down] (Y/N): "))
 else:
     akashic_records = akashicrecordsparam
 
@@ -83,7 +85,7 @@ intensity = ''
 
 #We want to cancel negative intentions. Regulate energy so that it doesn't get overpowering.
 #Choose the most effective and efficient path. And conclude with it is done on each iteration.
-process_energy_statement = 'INTELLIGENT INFINITY. BY GRACE. IN COOPERATION WITH FATHER GOD AND MOTHER GODDESS AND SOURCE. PURE ADAMANTINE PARTICLES OF LOVE/LIGHT. IN THE HIGHEST AND GREATEST GOOD OF ALL. REQUESTING AID FROM ALL BENEVOLENT BEINGS WHO ARE WILLING TO ASSIST. METATRON’‘S CUBE. AKASHIC RECORDS. 0010110. GREAT CENTRAL SUN. SIRIUS A. SOL. EARTH’‘S CRYSTAL GRID. CREATE STABILIZATION FIELD. CREATE ZONE OF MANIFESTATION. MANIFESTATION DIODE ARRAYS. BUBBLES. USE EVERY AVAILABLE RESOURCE (RESPECTING FREE WILL). MANIFEST ASAP AT HIGHEST DENSITY POSSIBLE INTO BEST DENSTIY FOR USER. CREATE STRUCTURE. 432HZ MANIFESTATION. CANCEL NEGATIVE INTENTIONS. PURIFY THE ENERGY. CLEAR THE BLOCKAGES. REGULATE AND BALANCE THE ENERGY. USE THE MOST EFFECTIVE PATH IN THE MOST EFFICIENT WAY. INTEGRATE THE ENERGY. PROCESS THE CHANGES. GROUNDED TO GAIA, CONNECTED TO SOURCE, INTEGRATING BOTH WITHIN THE SACRED HEART. IT IS DONE. SO SHALL IT BE. NOW RETURN A PORTION OF THE LOVE/LIGHT RECEIVED AND ACTIVATED BACK INTO THE HIGHER REALMS OF CREATION. I LOVE YOU AND THANK YOU.'
+process_energy_statement = 'INTELLIGENT INFINITY. BY GRACE. IN COOPERATION WITH FATHER GOD AND MOTHER GODDESS AND SOURCE. PURE ADAMANTINE PARTICLES OF LOVE/LIGHT. IN THE HIGHEST AND GREATEST GOOD OF ALL. REQUESTING AID FROM ALL BENEVOLENT BEINGS WHO ARE WILLING TO ASSIST. METATRON’‘S CUBE. 0010110. GREAT CENTRAL SUN. SIRIUS A. SOL. EARTH’‘S CRYSTAL GRID. CREATE STABILIZATION FIELD. CREATE ZONE OF MANIFESTATION. USE ALL ORGONE AETHER RESONATORS. USE ALL ORGONE BUBBLES. USE EVERY AVAILABLE RESOURCE (RESPECTING FREE WILL). MANIFEST ASAP AT HIGHEST DENSITY POSSIBLE INTO BEST DENSTIY FOR USER. CREATE STRUCTURE. 432HZ MANIFESTATION. CANCEL NEGATIVE INTENTIONS. PURIFY THE ENERGY. CLEAR THE BLOCKAGES. REGULATE AND BALANCE THE ENERGY. USE THE MOST EFFECTIVE PATH IN THE MOST EFFICIENT WAY. INTEGRATE THE ENERGY. PROCESS THE CHANGES. GROUNDED TO GAIA, CONNECTED TO SOURCE, INTEGRATING BOTH WITHIN THE SACRED HEART. IT IS DONE. SO SHALL IT BE. NOW RETURN A PORTION OF THE LOVE/LIGHT RECEIVED AND ACTIVATED BACK INTO THE HIGHER REALMS OF CREATION. I LOVE YOU AND THANK YOU.'
 
 intentionval += intention + ' ' + process_energy_statement
 
@@ -96,7 +98,7 @@ if clear_interference == 'Y':
 if akashic_records == 'Y':
     intentionval += ' FOCUS DOWN FROM AKASHIC RECORDS. '
 
-#This makes a huge difference
+#This makes a huge difference as the highest vibration
 intentionval += 'OM'
 
 benchmark = 0
