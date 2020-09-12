d6ec7e6c7710a600427e6cd68ffc108778db30c8
Thomas Sweet <admin@intentionrepeater.com>
2020-09-12 07:21:12 -0500

    Add files via upload

--- intention_repeater.py
+++ intention_repeater.py
@@ -6,6 +6,7 @@
 #Run using: python3 intention_repeater.py
 #Automated Example: python3 intention_repeater.py YYY8 "HH:MM:SS" "Intentions/Filename with Intentions"
 #Intention Repeater is powered by a Servitor (20 Years / 2000+ hours in the making)
+#Servitor Info: https://enlightenedstates.com/2017/04/07/servitor-just-powerful-spiritual-tool/
 #Website: https://www.intentionrepeater.com/
 #Forum: https://forums.intentionrepeater.com/
 #Licensed under GNU General Public License v3.0
